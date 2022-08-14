24d706ab16becbab4dfe99f4f720fcab5f30878c
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-08-14 16:54:15 +0300

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
 
-function App() {
+  const categories = [
+    {
+      id: 1,
+      title: "Diver Watches"
+    },
+    {
+      id: 2,
+      title: "Pilot Watches"
+    },
+    {
+      id: 3,
+      title: "Dress Watches"
+    },
+    {
+      id: 4,
+      title: "Skeleton Watches"
+    },
+    {
+      id: 5,
+      title: "Moonphase Watches"
+    },
+    {
+      id: 6,
+      title: "Limited Edition Watches"
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      {categories.map(({ title }) => (
+        <div className="category-container">
+          {/* img here */}
+          <div className="background-image" />
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }

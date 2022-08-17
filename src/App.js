f3ba07d429b01cd07caa0eba32f7a342bcbd3961
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-08-17 15:56:27 +0300

    Small routes architecture change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Home from './routes/homepage';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Home />
+    <Routes>
+      <Route path='/' index element={<Home />} />
+    </Routes>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}>
-
-        </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );

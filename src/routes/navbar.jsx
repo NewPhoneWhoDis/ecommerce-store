4e45f667215b45e06e2b8f309d1bf6863425b642
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-08-20 01:36:06 +0300

    fixed svg bug with the logo

--- src/routes/navbar.jsx
+++ src/routes/navbar.jsx
@@ -1,13 +1,13 @@
 import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
-import { ReactComponentElement as Logo } from "react";
+import { ReactComponent as Logo } from "../assets/crown.svg";
 
 const Navbar = () => {
     return (
         <Fragment>
             <div className="navigation">
                 <Link className="logo-container" to="/">
-                    <Logo />
+                    <Logo className="logo" />
                 </Link>
                 <div className="nav-links-container">
                     <Link className="nav-links" to="/shop">

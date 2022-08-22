7ca907e3da6b9d718b22adc03fb848992175d60b
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-08-22 19:06:56 +0300

    init dev

--- src/routes/navigation/navbar.jsx
+++ src/routes/navigation/navbar.jsx
@@ -2,8 +2,38 @@ import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./navbar.styles.scss";
+import {
+    AppBar,
+    Toolbar,
+    CssBaseline,
+    Typography,
+    makeStyles,
+} from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+    navlinks: {
+        marginLeft: theme.spacing(10),
+        display: "flex",
+    },
+    logo: {
+        flexGrow: "1",
+        cursor: "pointer",
+    },
+    link: {
+        textDecoration: "none",
+        color: "white",
+        fontSize: "20px",
+        marginLeft: theme.spacing(20),
+        "&:hover": {
+            color: "yellow",
+            borderBottom: "1px solid white",
+        },
+    },
+}));
 
 const Navbar = () => {
+    const classes = useStyles();
+
     return (
         <Fragment>
             <div className="navigation">
@@ -17,6 +47,12 @@ const Navbar = () => {
                     <span>|</span>
                     <Link className="nav-links" to="/sign-in">
                         Sign-in
+                    </Link>
+                    <Link className="nav-links" to="/About">
+                        About us
+                    </Link>
+                    <Link className="nav-links" to="/FAQ">
+                        FAQ
                     </Link>
                 </div>
             </div>

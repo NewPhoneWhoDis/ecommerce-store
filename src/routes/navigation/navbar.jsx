18e190e56e16b0b4ee0a9f6835e40a036bf17b47
Christian <88944722+NewPhoneWhoDis@users.noreply.github.com>
2022-08-25 21:58:14 +0300

    Navbar with material UI

--- src/routes/navigation/navbar.jsx
+++ src/routes/navigation/navbar.jsx
@@ -2,60 +2,62 @@ import { Outlet, Link } from "react-router-dom";
 import { Fragment } from "react";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import "./navbar.styles.scss";
-import {
-    AppBar,
-    Toolbar,
-    CssBaseline,
-    Typography,
-    makeStyles,
-} from "@mui/material";
-
-const useStyles = makeStyles((theme) => ({
-    navlinks: {
-        marginLeft: theme.spacing(10),
-        display: "flex",
-    },
-    logo: {
-        flexGrow: "1",
-        cursor: "pointer",
-    },
-    link: {
-        textDecoration: "none",
-        color: "white",
-        fontSize: "20px",
-        marginLeft: theme.spacing(20),
-        "&:hover": {
-            color: "yellow",
-            borderBottom: "1px solid white",
-        },
-    },
-}));
+import { AppBar, Toolbar, Tabs, Tab, Typography } from "@mui/material";
 
 const Navbar = () => {
-    const classes = useStyles();
-
     return (
         <Fragment>
-            <div className="navigation">
-                <Link className="logo-container" to="/">
-                    <Logo className="logo" />
-                </Link>
-                <div className="nav-links-container">
-                    <Link className="nav-links" to="/shop">
-                        Shop
-                    </Link>
-                    <span>|</span>
-                    <Link className="nav-links" to="/sign-in">
-                        Sign-in
-                    </Link>
-                    <Link className="nav-links" to="/About">
-                        About us
-                    </Link>
-                    <Link className="nav-links" to="/FAQ">
-                        FAQ
-                    </Link>
-                </div>
-            </div>
+            <AppBar sx={{ background: "#000" }}>
+                <Toolbar>
+                    <Typography>
+                        <Link className="logo-container" to="/">
+                            <Logo className="logo" />
+                        </Link>
+                    </Typography>
+                    <Tabs>
+                        <Link className="nav-links-container" to="/sign-in">
+                            <Tab
+                                sx={{
+                                    color: "white",
+                                    justifyContent: "flex-end",
+                                    alignItems: "center",
+                                }}
+                                label="Sign-in"
+                            />
+                        </Link>
+                        <Link className="nav-links-container" to="/shop">
+                            <Tab
+                                sx={{
+                                    color: "white",
+                                    justifyContent: "flex-end",
+                                    alignItems: "center",
+                                }}
+                                label="Shop"
+                            />
+                        </Link>
+                        <Link className="nav-links-container" to="/faq">
+                            <Tab
+                                sx={{
+                                    color: "white",
+                                    justifyContent: "flex-end",
+                                    alignItems: "center",
+                                }}
+                                label="FAQ"
+                            />
+                        </Link>
+                        <Link className="nav-links-container" to="/contact">
+                            <Tab
+                                sx={{
+                                    color: "white",
+                                    justifyContent: "flex-end",
+                                    alignItems: "center",
+                                }}
+                                label="Contact us"
+                            />
+                        </Link>
+                    </Tabs>
+                </Toolbar>
+            </AppBar>
             <Outlet />
         </Fragment>
     );

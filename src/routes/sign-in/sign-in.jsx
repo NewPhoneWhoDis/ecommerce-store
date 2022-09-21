1a92f13956447ed65082b2a0e12c46dc71b35389
NewPhoneWhoDis <kristiyanmerdzhov@Kristiyans-MacBook-Air.local>
2022-09-22 01:46:39 +0200

    Added sign in button

--- src/routes/sign-in/sign-in.jsx
+++ src/routes/sign-in/sign-in.jsx
@@ -1,7 +1,15 @@
+import { signInWithGooglePopup } from "../../util/firebase.utils";
+
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const user = await signInWithGooglePopup();
+    }
     return (
         <div>
             <h1>Sign in page</h1>
+            <button onClick={logGoogleUser}>
+                Sign in with Google
+            </button>
         </div>
     )
 }
